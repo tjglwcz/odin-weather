f3fa779a7e6679d159e56bb132f551c73250005f
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-16 18:04:40 +0200

    Create parseData and renderData functions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
-function checkWeather(city) {
-  fetch(
-    `http://api.weatherapi.com/v1/current.json?key=8a4396e00e97479f88f130836241604&q=${city}`
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+const apiKey = "8a4396e00e97479f88f130836241604";
+
+const formInput = document.querySelector("#formInput");
+const submitBtn = document.querySelector("button");
+
+const cityDisplay = document.querySelector("#cityDisplay");
+const conditionDisplay = document.querySelector("#conditionDisplay");
+const tempDisplay = document.querySelector("#tempDisplay");
+const feelsDisplay = document.querySelector("#feelsDisplay");
+const humidityDisplay = document.querySelector("#humidityDisplay");
+
+async function requestData(city) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error: Could not retrieve data.", error);
+    return null;
+  }
 }
+
+function parseData(data) {
+  return {
+    name: data.location.name,
+    temp: data.current.temp_c,
+    feelsLikeTemp: data.current.feelslike_c,
+    humidity: data.current.humidity,
+    condition: data.current.condition.text,
+  };
+}
+
+function renderData(data) {
+  cityDisplay.textContent = data.name;
+  conditionDisplay.textContent = data.condition;
+  tempDisplay.textContent = `${data.temp}°C`;
+  feelsDisplay.textContent = `Feels like ${data.feelsLikeTemp}°C`;
+  humidityDisplay.textContent = `Humidity: ${data.humidity}%`;
+}
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  requestData(formInput.value)
+    .then((data) => {
+      const newData = parseData(data);
+      renderData(newData);
+    })
+    .catch((error) => {
+      console.error("Error occurred:", error);
+    });
+});
